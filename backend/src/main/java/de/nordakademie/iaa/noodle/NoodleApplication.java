27a0e7528db0bb9d9615f0ff43690df299eb2956
Hans Rißer <hans.r.1999@gmail.com>
2020-10-23 19:19:29 +0200

    feat: :tada: Set up SpringBoot backend

---
+++ backend/src/main/java/de/nordakademie/iaa/noodle/NoodleApplication.java
@@ -0,0 +1,13 @@
+package de.nordakademie.iaa.noodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoodleApplication.class, args);
+	}
+
+}
