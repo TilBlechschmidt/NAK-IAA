0703170ca2cabe4b9303bec788ed2f8b04ba0bbc
Hans Rißer <hans.r.1999@gmail.com>
2020-10-23 19:19:29 +0200

    feat: :tada: Set up SpringBoot backend

---
+++ backend/src/test/java/de/nordakademie/iaa/noodle/NoodleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nordakademie.iaa.noodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
