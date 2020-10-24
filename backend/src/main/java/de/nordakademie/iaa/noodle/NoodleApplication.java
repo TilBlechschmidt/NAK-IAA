c89e95059fab1afcfaaae4242fb27de9bbd90e73
NoahPeeters <noah@noahpeeters.de>
2020-10-24 23:11:24 +0200

    feat: Add backend api model generation

--- backend/src/main/java/de/nordakademie/iaa/noodle/NoodleApplication.java
+++ backend/src/main/java/de/nordakademie/iaa/noodle/NoodleApplication.java
@@ -2,12 +2,12 @@ package de.nordakademie.iaa.noodle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class NoodleApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NoodleApplication.class, args);
 	}
-
 }
---
+++ backend/src/main/java/de/nordakademie/iaa/noodle/controller/ApiDocsController.java
@@ -0,0 +1,43 @@
+package de.nordakademie.iaa.noodle.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Home redirection to OpenAPI api documentation
+ */
+@Controller
+@Api(value = "ApiDocsController")
+public class ApiDocsController {
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:swagger-ui/index.html";
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Noodle")
+            .description("API of the Noodle planner")
+            .license("")
+            .licenseUrl("http://unlicense.org")
+            .termsOfServiceUrl("")
+            .version("0.1.0")
+            .build();
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("de.nordakademie.iaa.noodle.controller"))
+            .build()
+            .apiInfo(apiInfo());
+    }
+}
