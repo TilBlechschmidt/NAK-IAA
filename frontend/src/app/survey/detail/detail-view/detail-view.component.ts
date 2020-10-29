e7cfbf78391f92f09578b2b7f394ff09419e75c5
Hendrik Reiter <here@ppi.de>
2020-10-29 15:16:52 +0100

    :lipstick: change the theme

--- frontend/src/app/survey/detail/detail-view/detail-view.component.ts
+++ frontend/src/app/survey/detail/detail-view/detail-view.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailViewComponent implements OnInit {
 
+   isEdit: boolean = false;
+   title: string = "";
+   description: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleEdit() {
+      this.isEdit = !this.isEdit;
   }
 
 }
