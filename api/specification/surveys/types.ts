9b46ba3bac3349238f3ff9ba04016cadf87c2284
NoahPeeters <noah@noahpeeters.de>
2020-11-02 14:49:20 +0100

    Add creator to survey query response

--- api/specification/surveys/types.ts
+++ api/specification/surveys/types.ts
@@ -1,6 +1,7 @@
 import {DateTime, String} from "@airtasker/spot";
 import {Response} from './responses/types';
 import {Identifier} from "../types";
+import {IdentifiableUser} from "../account/types";
 
 /** Short title to be displayed in survey list (max. 64 characters) */
 export type SurveyTitle = String;
@@ -18,6 +19,9 @@ export interface SurveyMetadata {
 
     /** Text describing what this survey is about (max. 1024 characters) */
     description: String;
+
+    /** The user who created the survey. */
+    creator: IdentifiableUser;
 
     /** List of available timeslots for the survey */
     timeslots: Timeslot[];
