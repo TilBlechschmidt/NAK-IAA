dd99ff49e81d426f49890935ad2a808c8091659c
Hans Rißer <hans.r.1999@gmail.com>
2020-11-03 15:07:54 +0100

    feat: :construction: Add big Survey Query

--- backend/src/main/java/de/nordakademie/iaa/noodle/dao/SurveyRepository.java
+++ backend/src/main/java/de/nordakademie/iaa/noodle/dao/SurveyRepository.java
@@ -4,6 +4,7 @@ import de.nordakademie.iaa.noodle.model.Survey;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,12 +19,42 @@ public interface SurveyRepository {
     List<Survey> findAllByCreator_id(Long creatorId);
 
     @Query("""
-        SELECT s1 FROM Survey s1 WHERE ?1 NOT IN
+        SELECT s1 FROM Survey s1 WHERE :userID NOT IN
             (SELECT p.participant
             FROM Survey s LEFT OUTER JOIN Participation p ON p.survey=s
             WHERE s.id = s1.id )""")
-    List<Survey> findSurveysWhereUserHasNotParticipated(Long userID);
+    List<Survey> findSurveysWhereUserHasNotParticipated(@Param("userID") Long userID);
 
+
+    @Query("""
+                SELECT survey FROM Survey survey
+                    LEFT OUTER JOIN Participation participation ON participation.survey = survey
+                    WHERE
+                    (:participated IS NULL OR
+                        (participation.participant.id = :userID))
+                    AND
+                    (:completed IS NULL OR
+                        (survey.chosenTimeslot IS NOT NULL))
+                    AND
+                    (:owned IS NULL OR
+                        survey.creator.id = :userID)
+                    AND
+                    (:upcoming IS NULL OR
+                        (survey.chosenTimeslot IS NOT NULL AND survey.chosenTimeslot.start >= CURRENT_TIMESTAMP ))
+                    AND
+                     (:attentionRequired IS NULL OR
+                        (:userID NOT IN
+                            (SELECT p.participant
+                            FROM Survey s
+                            LEFT OUTER JOIN Participation p ON p.survey = s
+                            WHERE s.id = survey.id )))
+        """)
+    List<Survey> querySurvey(@Param("userID") Long userID,
+                             @Param("participated") Boolean didParticipateIn,
+                             @Param("completed") Boolean isCompleted,
+                             @Param("owned") Boolean isOwnSurvey,
+                             @Param("upcoming") Boolean isUpcoming,
+                             @Param("attentionRequired") Boolean requiresAttention);
 
     /**
      * @deprecated For extensibility Reasons, please use {@link #findSurveysThatNeedAttentionBy(Long)}}
@@ -42,7 +73,7 @@ public interface SurveyRepository {
         "participations.participant",
         "participations.response",
         "participations.response.responseTimeslots"
-        })
+    })
     Survey findById(Long id);
 
     Survey save(Survey toSave);
