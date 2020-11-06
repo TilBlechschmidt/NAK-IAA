cbee4f3e1ba03d86545ac1258c8cdb78505f1ef1
Hans Rißer <hans.r.1999@gmail.com>
2020-11-06 18:33:58 +0100

    test: :white_check_mark: Test ResponseController

--- backend/src/test/java/de/nordakademie/iaa/noodle/controller/ResponseControllerTest.java
+++ backend/src/test/java/de/nordakademie/iaa/noodle/controller/ResponseControllerTest.java
@@ -1,19 +1,25 @@
 package de.nordakademie.iaa.noodle.controller;
 
 import de.nordakademie.iaa.noodle.TestUtil;
+import de.nordakademie.iaa.noodle.api.model.CreateResponseRequest;
 import de.nordakademie.iaa.noodle.api.model.ResponseDTO;
 import de.nordakademie.iaa.noodle.mapper.ResponseMapper;
 import de.nordakademie.iaa.noodle.model.Response;
 import de.nordakademie.iaa.noodle.model.User;
 import de.nordakademie.iaa.noodle.services.ResponseService;
+import de.nordakademie.iaa.noodle.services.exceptions.ConflictException;
 import de.nordakademie.iaa.noodle.services.exceptions.EntityNotFoundException;
+import de.nordakademie.iaa.noodle.services.exceptions.ForbiddenOperationException;
+import de.nordakademie.iaa.noodle.services.exceptions.SemanticallyInvalidInputException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static de.nordakademie.iaa.noodle.TestUtil.assertEqualsResponseEntity;
+import static de.nordakademie.iaa.noodle.TestUtil.assertThrowsResponseStatusException;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -32,32 +38,87 @@ public class ResponseControllerTest {
     }
 
     @Test
-    public void testCreateResponse() {
-        TestUtil.skip();
-//        CreateResponseRequest request = mock(CreateResponseRequest.class);
-//        ResponseEntity<ResponseDTO> response = responseController.createResponse(42L, request);
-//        assertNull(response);
+    public void testCreateResponseDuplicate() throws EntityNotFoundException, ConflictException, SemanticallyInvalidInputException {
+        when(responseService.createResponse(any(), any(), any())).thenThrow(new ConflictException("testCreateResponse"));
+        CreateResponseRequest inputDTO = mock(CreateResponseRequest.class);
+        assertThrowsResponseStatusException(HttpStatus.CONFLICT, "testCreateResponse", () -> responseController.createResponse(42L, inputDTO));
+    }
+
+    @Test
+    public void testCreateResponseInvalidSemantics() throws EntityNotFoundException, ConflictException, SemanticallyInvalidInputException {
+        when(responseService.createResponse(any(), any(), any())).thenThrow(new SemanticallyInvalidInputException("testCreateResponse"));
+        CreateResponseRequest inputDTO = mock(CreateResponseRequest.class);
+        assertThrowsResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "testCreateResponse", () -> responseController.createResponse(42L, inputDTO));
+    }
+
+    @Test
+    public void testCreateResponseNotFound() throws EntityNotFoundException, ConflictException, SemanticallyInvalidInputException {
+        when(responseService.createResponse(any(), any(), any())).thenThrow(new EntityNotFoundException("testCreateResponse"));
+        CreateResponseRequest inputDTO = mock(CreateResponseRequest.class);
+        assertThrowsResponseStatusException(HttpStatus.NOT_FOUND, "testCreateResponse", () -> responseController.createResponse(42L, inputDTO));
+    }
+
+    @Test
+    public void testCreateResponse() throws EntityNotFoundException, SemanticallyInvalidInputException, ConflictException {
+        Response expectedResponse = mock(Response.class);
+        when(responseService.createResponse(any(), any(), any())).thenReturn(expectedResponse);
+
+        ResponseDTO expectedDTO = mock(ResponseDTO.class);
+        when(responseMapper.responseToDTO(same(expectedResponse), any())).thenReturn(expectedDTO);
+
+        CreateResponseRequest inputDTO = mock(CreateResponseRequest.class);
+        ResponseEntity<ResponseDTO> response = responseController.createResponse(42L, inputDTO);
+        assertEqualsResponseEntity(HttpStatus.CREATED, expectedDTO, response);
+    }
+
+    @Test
+    public void testQueryResponseNotFound() throws EntityNotFoundException {
+        when(responseService.queryResponse(any(), any())).thenThrow(new EntityNotFoundException("testQueryResponse"));
+        assertThrowsResponseStatusException(HttpStatus.NOT_FOUND, "testQueryResponse", () -> responseController.queryResponse(42L, 42L));
     }
 
     @Test
     public void testQueryResponse() throws EntityNotFoundException {
         Response inputResponse = mock(Response.class);
-        ResponseDTO inputResponseDTO = mock(ResponseDTO.class);
+        ResponseDTO expectedDTO = mock(ResponseDTO.class);
         when(responseService.queryResponse(42L, 43L)).thenReturn(inputResponse);
-        when(responseMapper.responseToDTO(inputResponse, authenticatedUser)).thenReturn(inputResponseDTO);
+        when(responseMapper.responseToDTO(inputResponse, authenticatedUser)).thenReturn(expectedDTO);
 
         ResponseEntity<ResponseDTO> response = responseController.queryResponse(42L, 43L);
-        ResponseDTO responseDTO = response.getBody();
-
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(inputResponseDTO, responseDTO);
+        assertEqualsResponseEntity(HttpStatus.OK, expectedDTO, response);
     }
 
     @Test
-    public void testUpdateResponse() {
-        TestUtil.skip();
-//        CreateResponseRequest request = mock(CreateResponseRequest.class);
-//        ResponseEntity<ResponseDTO> response = responseController.updateResponse(42L, 43L, request);
-//        assertNull(response);
+    public void testUpdateResponseInvalidSemantics() throws EntityNotFoundException, SemanticallyInvalidInputException, ForbiddenOperationException {
+        when(responseService.updateResponse(any(), any(), any(), any())).thenThrow(new SemanticallyInvalidInputException("testCreateResponse"));
+        CreateResponseRequest inputDTO = mock(CreateResponseRequest.class);
+        assertThrowsResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "testCreateResponse", () -> responseController.updateResponse(42L, 42L, inputDTO));
+    }
+
+    @Test
+    public void testUpdateResponseForbidden() throws EntityNotFoundException, SemanticallyInvalidInputException, ForbiddenOperationException {
+        when(responseService.updateResponse(any(), any(), any(), any())).thenThrow(new ForbiddenOperationException("testCreateResponse"));
+        CreateResponseRequest inputDTO = mock(CreateResponseRequest.class);
+        assertThrowsResponseStatusException(HttpStatus.FORBIDDEN, "testCreateResponse", () -> responseController.updateResponse(42L, 42L, inputDTO));
+    }
+
+    @Test
+    public void testUpdateResponseNotFound() throws EntityNotFoundException, SemanticallyInvalidInputException, ForbiddenOperationException {
+        when(responseService.updateResponse(any(), any(), any(), any())).thenThrow(new EntityNotFoundException("testCreateResponse"));
+        CreateResponseRequest inputDTO = mock(CreateResponseRequest.class);
+        assertThrowsResponseStatusException(HttpStatus.NOT_FOUND, "testCreateResponse", () -> responseController.updateResponse(42L, 42L, inputDTO));
+    }
+
+    @Test
+    public void testUpdateResponse() throws EntityNotFoundException, ForbiddenOperationException, SemanticallyInvalidInputException {
+        Response expectedResponse = mock(Response.class);
+        when(responseService.updateResponse(any(), any(), any(), any())).thenReturn(expectedResponse);
+
+        ResponseDTO expectedDTO = mock(ResponseDTO.class);
+        when(responseMapper.responseToDTO(same(expectedResponse), any())).thenReturn(expectedDTO);
+
+        CreateResponseRequest inputDTO = mock(CreateResponseRequest.class);
+        ResponseEntity<ResponseDTO> response = responseController.updateResponse(42L, 42L, inputDTO);
+        assertEqualsResponseEntity(HttpStatus.CREATED, expectedDTO, response);
     }
 }
