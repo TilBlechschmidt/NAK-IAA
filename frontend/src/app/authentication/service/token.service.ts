84da9ef234b61c2656e7e3c1646364aa45fdca1e
Til Blechschmidt <til@blechschmidt.de>
2020-11-14 13:55:59 +0100

    :children_crossing: Change localStorage for sessionStorage

--- frontend/src/app/authentication/service/token.service.ts
+++ frontend/src/app/authentication/service/token.service.ts
@@ -6,22 +6,24 @@ import {Jwt} from '../../api/models/jwt';
 })
 export class TokenService {
 
+    private static storage = sessionStorage;
+
     constructor() {
     }
 
     getToken(): Jwt | null {
-        return localStorage.getItem('jwt');
+        return TokenService.storage.getItem('jwt');
     }
 
     isAuthenticated(): boolean {
-        return localStorage.getItem('jwt') != null;
+        return TokenService.storage.getItem('jwt') != null;
     }
 
     deleteToken(): void {
-        localStorage.removeItem('jwt');
+        TokenService.storage.removeItem('jwt');
     }
 
     setToken(token: Jwt): void {
-        localStorage.setItem('jwt', token);
+        TokenService.storage.setItem('jwt', token);
     }
 }
