147ea8f676776ac5d21b9a807cb0ed1f67a1be69
Hendrik Reiter <here@ppi.de>
2020-11-15 15:38:01 +0100

    :bug: solve dashboard bugs

--- frontend/src/app/survey/tabs/abstract-survey-table/abstract-survey-table.component.ts
+++ frontend/src/app/survey/tabs/abstract-survey-table/abstract-survey-table.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {QuerySurveysResult} from '../../../api/models/query-surveys-result';
 import {SurveysService} from '../../../api/services/surveys.service';
+import {DateService} from '../../../date.service';
+import {TimeslotDto} from '../../../api/models/timeslot-dto';
 
 @Component({
     selector: 'app-abstract-survey-table',
@@ -12,11 +14,12 @@ export class AbstractSurveyTableComponent implements OnInit {
     @Input()
     filter: Filter = {};
 
-    displayedColumns: string[] = ['title', 'participantCount'];
+    displayedColumns: string[] = [];
     data: QuerySurveysResult[] = [];
     fetchError = false;
 
-    constructor(protected surveysService: SurveysService) {}
+    constructor(protected surveysService: SurveysService, private dateService: DateService) {
+    }
 
     ngOnInit(): void {
         this.surveysService.querySurveys({
@@ -26,9 +29,22 @@ export class AbstractSurveyTableComponent implements OnInit {
             isOwnSurvey: this.filter.isOwnSurvey,
             requiresAttention: this.filter.requiresAttention,
             acceptsSelectedTimeslot: this.filter.acceptsSelectedTimeslot
-        }).subscribe(next => this.data = next.surveys, error => {
+        }).subscribe(next => {
+            this.data = next.surveys;
+            this.displayedColumns = this.filter.acceptsSelectedTimeslot ?
+                ['title', 'participantCount', 'selectedTimeslot'] : ['title', 'participantCount'];
+        }, error => {
             this.fetchError = true;
         });
+    }
+
+    formatDate(selectedTimeslot: TimeslotDto): string {
+        const start = this.dateService.formatHumanReadable(selectedTimeslot.start);
+        if (selectedTimeslot.end) {
+            return start + ' - ' + this.dateService.formatHumanReadable(selectedTimeslot.end);
+        } else {
+            return start;
+        }
     }
 }
 
